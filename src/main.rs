34e67f6edebfb0718bdb83a9f140c2a6fed4656e
Sawyer Bergeron <sawyerbergeron@gmail.com>
2019-12-03 12:58:56 -0500

    Start basic wayland client tie-ins

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 //use 
+extern crate wayland_client;
 
 fn main() {
-    println!("Hello, world!");
+    // critical error if can't connect to display
+    let display = wayland_client::Display::connect_to_env().unwrap();
+
+    println!("got a display");
+    // display disconnects at end of scope
 }
